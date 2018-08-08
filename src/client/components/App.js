d04eec8a8ace265905ec0667bad06e8f9daad88f
Abe <absolutus11@gmail.com>
2018-08-09 03:26:34 +0400

    root path routing fixed

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -37,7 +37,7 @@ class App extends Component {
                   <Route path="/messages" component={Messages} />
                   <Route path="/event" component={NewEvent} />
                   <Route path="/friends" component={Friends} />
-                  <Route path="/" component={null} exact="true" />
+                  <Route path="/" component={null} exact />
                 </div>
               </HashRouter>
               <section className="section" />
--- src/client/components/views/Home.js
+++ src/client/components/views/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Button from '../Button';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="header">
+      <Button
+        className="home-close button"
+        id="home-close"
+        icon="fa fa-arrow-left icon-shadow"
+        onClick={() => {
+          console.log('home closed');
+        }}
+      />
+    </div>
+  );
 };
 
 export default Home;
